95d51c18d83ac1dcfff0d5ee86fc8351a0a6d2f3
Kos <koshuang@gmail.com>
2017-02-08 03:08:54 +0800

    test(job): add spec for MyJob

--- src/app/jobs/MyJob.js
+++ src/app/jobs/MyJob.js
@@ -9,6 +9,10 @@ export default class MyJob extends Job {
 
   async run(job) {
     // you should implement the run method.
+    this._log();
+  }
+
+  _log() {
     this.logger.debug(`run my job`);
   }
 
---
+++ test/app/jobs/MyJob.spec.js
@@ -0,0 +1,53 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import MyJob from 'jobs/MyJob';
+
+describe('Test MyJob', () => {
+  let sandbox;
+  let myJob;
+
+  const dummy = () => {};
+
+  beforeEach(async () => {
+    sandbox = sinon.sandbox.create();
+    myJob = new MyJob();
+    myJob.queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    myJob.queue.testMode.clear();
+    sandbox.restore();
+  });
+
+  describe('constructor', () => {
+    it('should create a queue', async() => {
+      expect(myJob.queue).be.not.undefined;
+    });
+  });
+
+  describe('when calling run', () => {
+    it('should call _log', async() => {
+      const stub = sandbox.stub(myJob, '_log')
+        .returns({
+          then: dummy,
+        });
+      myJob.run();
+
+      expect(stub.calledOnce).be.true;
+    });
+  });
+
+  describe('when calling _log', () => {
+    it('should calling logger.debug', async() => {
+      const stub = sandbox.stub(myJob.logger, 'debug')
+        .returns({
+          then: dummy,
+        });
+
+      myJob._log(dummy);
+
+      expect(stub.calledOnce).be.true;
+    });
+  });
+});
